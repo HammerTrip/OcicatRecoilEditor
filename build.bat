95ff4112775a2daad016d111bdae4611a2c409f9
taisnigs <48367687+pkowak@users.noreply.github.com>
2024-04-30 19:56:45 +0300

    2

--- build.bat
+++ build.bat
@@ -9,8 +9,6 @@ mkdir build
 cmake -GNinja .
 ninja
 
-xcopy .\lib\SFML-2.6.1\bin\openal32.dll build\ /D /Y /C /K
-xcopy .\lib\SFML-2.6.1\bin\sfml-audio-d-2.dll build\ /D /Y /C /K
 xcopy .\lib\SFML-2.6.1\bin\sfml-graphics-d-2.dll build\ /D /Y /C /K
 xcopy .\lib\SFML-2.6.1\bin\sfml-system-d-2.dll build\ /D /Y /C /K
 xcopy .\lib\SFML-2.6.1\bin\sfml-window-d-2.dll build\ /D /Y /C /K
